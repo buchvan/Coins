c0b86920e3d725adb63fcc1449e52a8da2ee12e1
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-06 10:36:29 +0600

    [*] add class of CoefficientlyFeature

---
+++ src/main/java/io/neolab/internship/coins/server/feature/CoefficientlyFeature.java
@@ -0,0 +1,40 @@
+package io.neolab.internship.coins.server.feature;
+
+import java.util.Objects;
+
+/**
+ * Особенность с целочисленным коэффициентом пары раса-тип_клетки (Race-CellType)
+ */
+public class CoefficientlyFeature extends Feature implements ICoefficientlyFeature {
+    private final int coefficient;
+
+    public CoefficientlyFeature(final FeatureType type, final int coefficient) {
+        super(type);
+        this.coefficient = coefficient;
+    }
+
+    public int getCoefficient() {
+        return coefficient;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        CoefficientlyFeature that = (CoefficientlyFeature) o;
+        return coefficient == that.coefficient;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), coefficient);
+    }
+
+    @Override
+    public String toString() {
+        return "CoefficientlyFeature{" +
+                "coefficient=" + coefficient +
+                '}';
+    }
+}
