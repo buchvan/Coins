979ae39d69aa3b035e311b1abd83f93ef77a09eb
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-06 11:19:42 +0600

    [+] add class Pair

---
+++ src/main/java/io/neolab/internship/coins/Pair.java
@@ -0,0 +1,54 @@
+package io.neolab.internship.coins;
+
+import java.util.Objects;
+
+/**
+ * Класс пара
+ */
+public class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
