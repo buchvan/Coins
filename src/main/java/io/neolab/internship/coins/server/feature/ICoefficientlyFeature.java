b88a8d7f140a92abffde6e87c1f8f1945c5283b7
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-06 10:35:18 +0600

    [*] add interfaces for Feature and CoefficientlyFeature

---
+++ src/main/java/io/neolab/internship/coins/server/feature/ICoefficientlyFeature.java
@@ -0,0 +1,8 @@
+package io.neolab.internship.coins.server.feature;
+
+/**
+ * Интерфейс особенности с целочисленным коэффициентом пары раса-тип_клетки (Race-CellType)
+ */
+public interface ICoefficientlyFeature extends IFeature {
+    int getCoefficient();
+}
