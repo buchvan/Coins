4d20fceb2e7ae06feae5a607ef6cbf3533ec19dc
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-07 12:08:21 +0600

    Merge remote-tracking branch 'remotes/origin/develop' into bim_server

---
+++ src/main/java/io/neolab/internship/coins/server/feature/IFeature.java
@@ -0,0 +1,8 @@
+package io.neolab.internship.coins.server.feature;
+
+/**
+ * Интерфейс особенности пары раса-тип_клетки (Race-CellType)
+ */
+public interface IFeature {
+    FeatureType getType();
+}
