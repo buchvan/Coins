ea72a0ebc0a635321659de4d23b13301a7ca6398
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-07 12:08:28 +0600

    Merge remote-tracking branch 'remotes/origin/develop' into bim_server

---
+++ src/main/java/io/neolab/internship/coins/server/game/board/Unit.java
@@ -0,0 +1,42 @@
+package io.neolab.internship.coins.server.game.board;
+
+import java.util.Objects;
+
+public class Unit {
+    private int id;
+
+    public Unit(int id) {
+        this.id = id;
+    }
+
+    public Unit() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Unit)) return false;
+        Unit unit = (Unit) o;
+        return getId() == unit.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Unit{" +
+                "id=" + id +
+                '}';
+    }
+}
