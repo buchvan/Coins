dd083013aea56f6da06ea21e828fe3890f9cd860
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-08 12:03:30 +0600

    [*] Added id generator

---
+++ src/main/java/io/neolab/internship/coins/utils/IdGenerator.java
@@ -0,0 +1,10 @@
+package io.neolab.internship.coins.utils;
+
+public class IdGenerator {
+    private static int currentId = 0;
+
+    public static int getCurrentId() {
+        currentId++;
+        return currentId;
+    }
+}
