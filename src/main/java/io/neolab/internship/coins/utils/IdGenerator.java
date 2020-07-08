840ea3539fe058e9616039c11b1dc4f2588462c6
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-08 12:12:26 +0600

    [R] Refactor id generator

--- src/main/java/io/neolab/internship/coins/utils/IdGenerator.java
+++ src/main/java/io/neolab/internship/coins/utils/IdGenerator.java
@@ -4,7 +4,6 @@ public class IdGenerator {
     private static int currentId = 0;
 
     public static int getCurrentId() {
-        currentId++;
-        return currentId;
+        return ++currentId;
     }
 }
