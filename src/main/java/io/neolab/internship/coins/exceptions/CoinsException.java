a76ac81a8d07c5624b3883872e69471bc2ed53c8
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-08 16:46:02 +0600

    [R] Refactor

--- src/main/java/io/neolab/internship/coins/exceptions/CoinsException.java
+++ src/main/java/io/neolab/internship/coins/exceptions/CoinsException.java
@@ -2,7 +2,6 @@ package io.neolab.internship.coins.exceptions;
 
 public class CoinsException {
     private ErrorCode errorCode;
-    private String param;
 
     public CoinsException(ErrorCode errorCode) {
         this.errorCode = errorCode;
