1e009e130b55f50babeb0e43749e7353940f23af
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-09 13:25:55 +0600

    [t] add beginning for SelfPlay tests

---
+++ src/test/java/io/neolab/internship/coins/server/game/SelfPlayTest.java
@@ -0,0 +1,11 @@
+package io.neolab.internship.coins.server.game;
+
+import org.junit.Test;
+
+// TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+public class SelfPlayTest {
+    @Test
+    public void testInitBoard() {
+
+    }
+}
