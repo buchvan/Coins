e9c3e642db40806ab9c78001a50069a9c13d3803
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-14 12:25:22 +0600

    [R] add comments

--- src/main/java/io/neolab/internship/coins/utils/LogCleaner.java
+++ src/main/java/io/neolab/internship/coins/utils/LogCleaner.java
@@ -8,6 +8,12 @@ public class LogCleaner {
     private static final String propertyName = "LOG_DIRECTORY";
     private static final int LOGS_MAX_COUNT = 10;
 
+    /**
+     * Загружает название директории с логами из logback
+     *
+     * @return значение свойства propertyName
+     * @throws IOException при ошибке чтения из файла logback
+     */
     private static String loadLogDirectory() throws IOException {
         final String searchingLine = "    <property name=\"" + propertyName + "\" ";
         String line;
@@ -19,6 +25,11 @@ public class LogCleaner {
         return line.split("\"")[3];
     }
 
+    /**
+     * Удаляет все логи из директории LOG_DIRECTORY при условии, что их число там больше, чем LOGS_MAX_COUNT
+     *
+     * @throws IOException при ошибке чтения из файла logback
+     */
     public static void clean() throws IOException {
         final String LOG_DIRECTORY = loadLogDirectory();
         final File logDir = new File(LOG_DIRECTORY);
