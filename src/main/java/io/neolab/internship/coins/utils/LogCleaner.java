a838daabff66cb2344b84147f16695dfdd72d535
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-14 11:44:18 +0600

    [R] refactoring for LogCleaner

--- src/main/java/io/neolab/internship/coins/utils/LogCleaner.java
+++ src/main/java/io/neolab/internship/coins/utils/LogCleaner.java
@@ -4,25 +4,27 @@ import java.io.*;
 import java.util.Arrays;
 
 public class LogCleaner {
+    private static final String logback = "src/main/resources/logback.xml";
+    private static final String propertyName = "LOG_DIRECTORY";
     private static final int LOGS_MAX_COUNT = 10;
 
-    public static void clean() throws IOException {
-        final String logback = "src/main/resources/logback.xml";
+    private static String loadLogDirectory() throws IOException {
+        final String searchingLine = "    <property name=\"" + propertyName + "\" ";
         String line;
         try (final BufferedReader bufferedReader = new BufferedReader(new FileReader(logback))) {
             do {
                 line = bufferedReader.readLine();
-            } while (!line.startsWith("    <property name=\"LOG_DIRECTORY\" "));
+            } while (!line.startsWith(searchingLine));
         }
+        return line.split("\"")[3];
+    }
 
-
-        // get the property value and print it out
-        final String LOG_DIRECTORY = line.split("\"")[3];
-
+    public static void clean() throws IOException {
+        final String LOG_DIRECTORY = loadLogDirectory();
         final File logDir = new File(LOG_DIRECTORY);
         if (logDir.exists()) {
             final File[] logs = logDir.listFiles();
-            if (logs != null && logs.length >= LOGS_MAX_COUNT) {
+            if (logs != null && logs.length > LOGS_MAX_COUNT) {
                 Arrays.stream(logs).forEach(File::delete);
             }
         }
