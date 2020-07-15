249da97554e9660f4d4797c1c53a9024b24616d9
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-15 12:41:22 +0600

    [*] update Communication

--- src/main/java/io/neolab/internship/coins/common/Communication.java
+++ src/main/java/io/neolab/internship/coins/common/Communication.java
@@ -2,16 +2,25 @@ package io.neolab.internship.coins.common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.neolab.internship.coins.server.game.IGame;
+import io.neolab.internship.coins.common.answer.Answer;
+import io.neolab.internship.coins.common.question.Question;
 
 public class Communication {
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    public static String serialize(final IGame game) throws JsonProcessingException {
-        return mapper.writeValueAsString(game);
+    public static String serializeQuestion(final Question question) throws JsonProcessingException {
+        return mapper.writeValueAsString(question);
     }
 
-    public static IGame deserialize(final String json) throws JsonProcessingException {
-        return mapper.readValue(json, IGame.class);
+    public static Question deserializeQuestion(final String json) throws JsonProcessingException {
+        return mapper.readValue(json, Question.class);
+    }
+
+    public static String serializeAnswer(final Answer answer) throws JsonProcessingException {
+        return mapper.writeValueAsString(answer);
+    }
+
+    public static Answer deserializeAnswer(final String json) throws JsonProcessingException {
+        return mapper.readValue(json, Answer.class);
     }
 }
