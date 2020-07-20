a7cb26018684edac4bc964b01f33d71702c8b87f
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-20 14:33:37 +0600

    [r] add comments

--- src/main/java/io/neolab/internship/coins/client/IClient.java
+++ src/main/java/io/neolab/internship/coins/client/IClient.java
@@ -5,5 +5,11 @@ import io.neolab.internship.coins.common.question.Question;
 import io.neolab.internship.coins.exceptions.CoinsException;
 
 public interface IClient {
+    /**
+     * Взять ответ на вопрос
+     * @param question - вопрос
+     * @return ответ на вопрос
+     * @throws CoinsException в случае неизвестного типа вопроса
+     */
     Answer getAnswer(final Question question) throws CoinsException;
 }
--- src/main/java/io/neolab/internship/coins/common/Communication.java
+++ src/main/java/io/neolab/internship/coins/common/Communication.java
@@ -7,41 +7,96 @@ import io.neolab.internship.coins.common.question.GameOverQuestion;
 import io.neolab.internship.coins.common.question.PlayerQuestion;
 import io.neolab.internship.coins.common.question.Question;
 
+/**
+ * Модуль сериализации-десериализации
+ */
 public class Communication {
     private static final ObjectMapper mapper = new ObjectMapper();
 
+    /**
+     * Сериализация вопроса
+     *
+     * @param question - вопрос
+     * @return json-строку - сериализованный вопрос
+     * @throws JsonProcessingException, если writeValueAsString this throws
+     */
     public static String serializeQuestion(final Question question) throws JsonProcessingException {
         return mapper.writeValueAsString(question);
     }
 
-    public static Question deserializeQuestion(final String json) throws JsonProcessingException {
-        return mapper.readValue(json, Question.class);
-    }
-
+    /**
+     * Десериализация вопроса игроку
+     *
+     * @param json - json-строка - сериализованный вопрос
+     * @return вопрос игроку
+     * @throws JsonProcessingException, если readValue this throws
+     */
     public static PlayerQuestion deserializePlayerQuestion(final String json) throws JsonProcessingException {
         return mapper.readValue(json, PlayerQuestion.class);
     }
 
+    /**
+     * Десериализация вопроса в конце игры
+     *
+     * @param json - json-строка - сериализованный вопрос
+     * @return вопрос в конце игры
+     * @throws JsonProcessingException, если readValue this throws
+     */
     public static GameOverQuestion deserializeGameOverQuestion(final String json) throws JsonProcessingException {
         return mapper.readValue(json, GameOverQuestion.class);
     }
 
+    /**
+     * Сериализация ответа
+     *
+     * @param answer - ответ
+     * @return json-строку - сериализованный ответ
+     * @throws JsonProcessingException, если writeValueAsString this throws
+     */
     public static String serializeAnswer(final Answer answer) throws JsonProcessingException {
         return mapper.writeValueAsString(answer);
     }
 
+    /**
+     * Десериализация ответа на вопрос захвата клетки
+     *
+     * @param json - json-строка - сериализованный ответ на вопрос захвата клетки
+     * @return ответ на вопрос захвата клетки
+     * @throws JsonProcessingException, если readValue this throws
+     */
     public static CatchCellAnswer deserializeCatchCellAnswer(final String json) throws JsonProcessingException {
         return mapper.readValue(json, CatchCellAnswer.class);
     }
 
+    /**
+     * Десериализация ответа на вопрос смены расы
+     *
+     * @param json - json-строка - сериализованный ответ на вопрос смены расы
+     * @return ответ на вопрос смены расы
+     * @throws JsonProcessingException, если readValue this throws
+     */
     public static ChangeRaceAnswer deserializeChangeRaceAnswer(final String json) throws JsonProcessingException {
         return mapper.readValue(json, ChangeRaceAnswer.class);
     }
 
+    /**
+     * Десериализация ответа на вопрос ухода в упадок
+     *
+     * @param json - json-строка - сериализованный ответ на вопрос ухода в упадок
+     * @return ответ на вопрос ухода в упадок
+     * @throws JsonProcessingException, если readValue this throws
+     */
     public static DeclineRaceAnswer deserializeDeclineRaceAnswer(final String json) throws JsonProcessingException {
         return mapper.readValue(json, DeclineRaceAnswer.class);
     }
 
+    /**
+     * Десериализация ответа на вопрос распределения юнитов
+     *
+     * @param json - json-строка - сериализованный ответ на вопрос распределения юнитов
+     * @return ответ на вопрос распределения юнитов
+     * @throws JsonProcessingException, если readValue this throws
+     */
     public static DistributionUnitsAnswer deserializeDistributionUnitsAnswer(final String json)
             throws JsonProcessingException {
         return mapper.readValue(json, DistributionUnitsAnswer.class);
