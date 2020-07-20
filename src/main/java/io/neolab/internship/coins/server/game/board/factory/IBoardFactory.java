422869a6a2a6d4baa3039651f3dddf139e90a476
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-20 21:18:58 +0600

    [r] add annotations in board/factory

--- src/main/java/io/neolab/internship/coins/server/game/board/factory/IBoardFactory.java
+++ src/main/java/io/neolab/internship/coins/server/game/board/factory/IBoardFactory.java
@@ -2,7 +2,8 @@ package io.neolab.internship.coins.server.game.board.factory;
 
 import io.neolab.internship.coins.exceptions.CoinsException;
 import io.neolab.internship.coins.server.game.board.IBoard;
+import org.jetbrains.annotations.NotNull;
 
 public interface IBoardFactory {
-    IBoard generateBoard(int width, int height) throws CoinsException;
+    @NotNull IBoard generateBoard(int width, int height) throws CoinsException;
 }
