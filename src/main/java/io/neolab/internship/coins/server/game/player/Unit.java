f99b7617c1861c8120c6e7401238d219df697caa
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-20 14:23:25 +0600

    [r] delete death code

--- src/main/java/io/neolab/internship/coins/server/game/player/Unit.java
+++ src/main/java/io/neolab/internship/coins/server/game/player/Unit.java
@@ -2,8 +2,6 @@ package io.neolab.internship.coins.server.game.player;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-//import io.neolab.internship.coins.common.deserialize.UnitDeserializer;
-//import io.neolab.internship.coins.common.serialize.UnitSerializer;
 import io.neolab.internship.coins.utils.IdGenerator;
 
 import java.io.Serializable;
