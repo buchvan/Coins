823296d15d30533c477f27aafc60095a2d6f3ba1
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-20 14:21:53 +0600

    [r] delete unused imports

--- src/main/java/io/neolab/internship/coins/client/IClient.java
+++ src/main/java/io/neolab/internship/coins/client/IClient.java
@@ -4,8 +4,6 @@ import io.neolab.internship.coins.common.answer.Answer;
 import io.neolab.internship.coins.common.question.Question;
 import io.neolab.internship.coins.exceptions.CoinsException;
 
-import java.io.IOException;
-
 public interface IClient {
     Answer getAnswer(final Question question) throws CoinsException;
 }
