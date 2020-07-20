2a43e6f5c057e5e64738b4f085b24774348b7217
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-20 12:49:39 +0600

    [r] package player

--- src/main/java/io/neolab/internship/coins/server/game/player/Player.java
+++ src/main/java/io/neolab/internship/coins/server/game/player/Player.java
@@ -1,4 +1,4 @@
-package io.neolab.internship.coins.server.game;
+package io.neolab.internship.coins.server.game.player;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
