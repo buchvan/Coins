9da93aefa487c5ec3800ea35e0b265ae73b9ae60
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-21 08:12:58 +0600

    [r] update Communication

--- src/main/java/io/neolab/internship/coins/common/serialization/Communication.java
+++ src/main/java/io/neolab/internship/coins/common/serialization/Communication.java
@@ -3,7 +3,6 @@ package io.neolab.internship.coins.common.serialization;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.neolab.internship.coins.common.answer.*;
-import io.neolab.internship.coins.common.question.PlayerQuestion;
 import io.neolab.internship.coins.common.question.Question;
 
 public class Communication {
@@ -17,28 +16,11 @@ public class Communication {
         return mapper.readValue(json, Question.class);
     }
 
-    public static PlayerQuestion deserializeGameQuestion(final String json) throws JsonProcessingException {
-        return mapper.readValue(json, PlayerQuestion.class);
-    }
-
     public static String serializeAnswer(final Answer answer) throws JsonProcessingException {
         return mapper.writeValueAsString(answer);
     }
 
-    public static CatchCellAnswer deserializeCatchCellAnswer(final String json) throws JsonProcessingException {
-        return mapper.readValue(json, CatchCellAnswer.class);
-    }
-
-    public static ChangeRaceAnswer deserializeChangeRaceAnswer(final String json) throws JsonProcessingException {
-        return mapper.readValue(json, ChangeRaceAnswer.class);
-    }
-
-    public static DeclineRaceAnswer deserializeDeclineRaceAnswer(final String json) throws JsonProcessingException {
-        return mapper.readValue(json, DeclineRaceAnswer.class);
-    }
-
-    public static DistributionUnitsAnswer deserializeDistributionUnitsAnswer(final String json)
-            throws JsonProcessingException {
-        return mapper.readValue(json, DistributionUnitsAnswer.class);
+    public static Answer deserializeAnswer(final String json) throws JsonProcessingException {
+        return mapper.readValue(json, Answer.class);
     }
 }
