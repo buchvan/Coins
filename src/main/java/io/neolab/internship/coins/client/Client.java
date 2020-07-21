139b87d0db1c06ef6950740617a22103c8f5c3ef
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-21 09:24:58 +0600

    [R] refactor switch/case

--- src/main/java/io/neolab/internship/coins/client/Client.java
+++ src/main/java/io/neolab/internship/coins/client/Client.java
@@ -55,31 +55,31 @@ public class Client implements IClient {
     @Override
     public Answer getAnswer(final Question question) throws CoinsException, IOException {
         switch (question.getQuestionType()) {
-            case CATCH_CELL -> {
+            case CATCH_CELL: {
                 final PlayerQuestion playerQuestion = (PlayerQuestion) question;
                 LOGGER.info("Catch cell question: {} ", playerQuestion);
                 return new CatchCellAnswer(
                         simpleBot.chooseCatchingCell(playerQuestion.getPlayer(), playerQuestion.getGame()));
             }
-            case DISTRIBUTION_UNITS -> {
+            case DISTRIBUTION_UNITS: {
                 final PlayerQuestion playerQuestion = (PlayerQuestion) question;
                 LOGGER.info("Distribution units question: {} ", playerQuestion);
                 return new DistributionUnitsAnswer(
                         simpleBot.distributionUnits(playerQuestion.getPlayer(), playerQuestion.getGame()));
             }
-            case DECLINE_RACE -> {
+            case DECLINE_RACE: {
                 final PlayerQuestion playerQuestion = (PlayerQuestion) question;
                 LOGGER.info("Decline race question: {} ", playerQuestion);
                 return new DeclineRaceAnswer(
                         simpleBot.declineRaceChoose(playerQuestion.getPlayer(), playerQuestion.getGame()));
             }
-            case CHANGE_RACE -> {
+            case CHANGE_RACE: {
                 final PlayerQuestion playerQuestion = (PlayerQuestion) question;
                 LOGGER.info("Change race question: {} ", playerQuestion);
                 return new ChangeRaceAnswer(
                         simpleBot.chooseRace(playerQuestion.getPlayer(), playerQuestion.getGame()));
             }
-            case GAME_OVER -> { // TODO: вывод результатов
+            case GAME_OVER: { // TODO: вывод результатов
                 LOGGER.info("Game over question: {} ", question);
                 String input;
                 do {
