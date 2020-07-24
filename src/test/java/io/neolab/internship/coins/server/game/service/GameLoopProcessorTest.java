1473f52e409d710c3fa643493b611bc8e799f6b9
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-24 15:39:47 +0600

    [*] add some tests

--- src/test/java/io/neolab/internship/coins/server/game/service/GameLoopProcessorTest.java
+++ src/test/java/io/neolab/internship/coins/server/game/service/GameLoopProcessorTest.java
@@ -1,6 +1,18 @@
 package io.neolab.internship.coins.server.game.service;
 
+import io.neolab.internship.coins.exceptions.CoinsException;
+import io.neolab.internship.coins.server.game.IGame;
+import io.neolab.internship.coins.server.game.board.Cell;
+import io.neolab.internship.coins.server.game.board.CellType;
+import io.neolab.internship.coins.server.game.player.Player;
+import io.neolab.internship.coins.server.game.player.Race;
+import io.neolab.internship.coins.server.service.TestUtils;
 import org.junit.Test;
+
+import java.util.Set;
+
+import static io.neolab.internship.coins.server.game.service.GameInitializer.gameInit;
+import static org.junit.Assert.assertEquals;
 
 public class GameLoopProcessorTest {
 
@@ -9,8 +21,16 @@ public class GameLoopProcessorTest {
      * каждый захваченный регион с грибами приносит в конце хода призовую монетку расе грибов
      */
     @Test
-    public void updateCoinsCountWithMushroomFeatures() {
+    public void updateCoinsCountWithMushroomFeatures() throws CoinsException {
+        IGame game = gameInit(2,2,2);
+        Player somePlayer = TestUtils.getSomePlayer(game);
+        somePlayer.setRace(Race.MUSHROOM);
+        somePlayer.setCoins(0);
+        Set<Cell> playerFeudalCells = game.getFeudalToCells().get(somePlayer);
+        playerFeudalCells.add(new Cell(CellType.MUSHROOM));
 
+        GameLoopProcessor.updateCoinsCount(somePlayer, playerFeudalCells, game.getGameFeatures(), game.getBoard());
+        assertEquals(2, somePlayer.getCoins());
     }
 
     //TODO
@@ -18,8 +38,17 @@ public class GameLoopProcessorTest {
      *  каждая отдельная группа регионов приносит одну дополнительную монетку расе эльфов
      */
     @Test
-    public void updateCoinsCountWithElfFeatures() {
+    public void updateCoinsCountWithElfFeatures() throws CoinsException {
+        IGame game = gameInit(2,2,2);
+        Player somePlayer = TestUtils.getSomePlayer(game);
+        somePlayer.setRace(Race.ELF);
+        somePlayer.setCoins(0);
+        Set<Cell> playerFeudalCells = game.getFeudalToCells().get(somePlayer);
+        playerFeudalCells.add(new Cell(CellType.MUSHROOM));
+        playerFeudalCells.add(new Cell(CellType.LAND));
 
+        GameLoopProcessor.updateCoinsCount(somePlayer, playerFeudalCells, game.getGameFeatures(), game.getBoard());
+        assertEquals(4, somePlayer.getCoins());
     }
 
     //TODO
@@ -27,17 +56,35 @@ public class GameLoopProcessorTest {
      * проверка того, что в произвольном случае монетки считаются верно
      */
     @Test
-    public void updateCoinsCountRightCoinsAmount() {
+    public void updateCoinsCountRightCoinsAmount() throws CoinsException {
+        IGame game = gameInit(2,2,2);
+        Player somePlayer = TestUtils.getSomePlayer(game);
+        somePlayer.setRace(Race.UNDEAD);
+        somePlayer.setCoins(0);
+        Set<Cell> playerFeudalCells = game.getFeudalToCells().get(somePlayer);
+        playerFeudalCells.add(new Cell(CellType.MUSHROOM));
+        playerFeudalCells.add(new Cell(CellType.LAND));
 
+        GameLoopProcessor.updateCoinsCount(somePlayer, playerFeudalCells, game.getGameFeatures(), game.getBoard());
+        assertEquals(2, somePlayer.getCoins());
     }
 
-    //TODO
+    //FIXME: failed
     /*
      * любая раса кроме амфибий не получает монетки за захват воды
      */
     @Test
-    public void updateCoinsCountWaterFeature() {
+    public void updateCoinsCountWaterFeature() throws CoinsException {
+        IGame game = gameInit(2,2,2);
+        Player somePlayer = TestUtils.getSomePlayer(game);
+        somePlayer.setRace(Race.AMPHIBIAN);
+        somePlayer.setCoins(0);
+        Set<Cell> playerFeudalCells = game.getFeudalToCells().get(somePlayer);
+        playerFeudalCells.add(new Cell(CellType.WATER));
+        playerFeudalCells.add(new Cell(CellType.LAND));
 
+        GameLoopProcessor.updateCoinsCount(somePlayer, playerFeudalCells, game.getGameFeatures(), game.getBoard());
+        assertEquals(1, somePlayer.getCoins());
     }
 
     //TODO:
@@ -45,8 +92,17 @@ public class GameLoopProcessorTest {
      * амфибии могут захватить воду
      */
     @Test
-    public void updateCoinsCountWaterAmphibianFeature() {
-        
+    public void updateCoinsCountWaterAmphibianFeature() throws CoinsException {
+        IGame game = gameInit(2,2,2);
+        Player somePlayer = TestUtils.getSomePlayer(game);
+        somePlayer.setRace(Race.AMPHIBIAN);
+        somePlayer.setCoins(0);
+        Set<Cell> playerFeudalCells = game.getFeudalToCells().get(somePlayer);
+        playerFeudalCells.add(new Cell(CellType.WATER));
+        playerFeudalCells.add(new Cell(CellType.LAND));
+
+        GameLoopProcessor.updateCoinsCount(somePlayer, playerFeudalCells, game.getGameFeatures(), game.getBoard());
+        assertEquals(2, somePlayer.getCoins());
     }
 
 }
