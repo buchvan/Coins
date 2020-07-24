d04babea3fc451f1e9f1ca51937dfd46db561ce6
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-24 15:15:34 +0600

    [*] branch init

---
+++ src/test/java/io/neolab/internship/coins/server/game/service/GameLoopProcessorTest.java
@@ -0,0 +1,52 @@
+package io.neolab.internship.coins.server.game.service;
+
+import org.junit.Test;
+
+public class GameLoopProcessorTest {
+
+    //TODO
+    /*
+     * каждый захваченный регион с грибами приносит в конце хода призовую монетку расе грибов
+     */
+    @Test
+    public void updateCoinsCountWithMushroomFeatures() {
+
+    }
+
+    //TODO
+    /*
+     *  каждая отдельная группа регионов приносит одну дополнительную монетку расе эльфов
+     */
+    @Test
+    public void updateCoinsCountWithElfFeatures() {
+
+    }
+
+    //TODO
+    /*
+     * проверка того, что в произвольном случае монетки считаются верно
+     */
+    @Test
+    public void updateCoinsCountRightCoinsAmount() {
+
+    }
+
+    //TODO
+    /*
+     * любая раса кроме амфибий не получает монетки за захват воды
+     */
+    @Test
+    public void updateCoinsCountWaterFeature() {
+
+    }
+
+    //TODO:
+    /*
+     * амфибии могут захватить воду
+     */
+    @Test
+    public void updateCoinsCountWaterAmphibianFeature() {
+        
+    }
+
+}
