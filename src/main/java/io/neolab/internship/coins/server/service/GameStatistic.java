ef05eef53811a0979542f4597e080cb84c26a8d1
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-29 09:59:27 +0600

    [+] Добавила комментарии к методам

--- src/main/java/io/neolab/internship/coins/server/service/GameStatistic.java
+++ src/main/java/io/neolab/internship/coins/server/service/GameStatistic.java
@@ -2,25 +2,31 @@ package io.neolab.internship.coins.server.service;
 
 import io.neolab.internship.coins.server.game.player.Player;
 
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static io.neolab.internship.coins.server.service.GameInitializer.initTestPlayers;
-import static io.neolab.internship.coins.server.service.SelfPlay.*;
+import static io.neolab.internship.coins.server.service.SelfPlay.selfPlayByPlayers;
 
+/**
+ * Класс, обеспечивающий сбор стастистики(процент побед и поражений)
+ */
+//FIXME: на большом количество итераций в конце игры у игроков слишком много монет(несколько тысяч)
 public class GameStatistic {
 
-    // или бот вместо Player ?
     private static Map<Player, Integer> playersStatistic = new HashMap<>();
+    private static final int GAME_AMOUNT = 3;
+    private static final int PLAYERS_AMOUNT = 2;
 
 
-    public static void play(final int gameAmount) {
+    public static void play() {
         final List<Player> players = initPlayers();
         initStatisticMap(players);
-        for (int i = 0; i < gameAmount; i++) {
-            List<Player> winners;
+        for (int i = 0; i < GAME_AMOUNT; i++) {
+            final List<Player> winners;
             winners = selfPlayByPlayers(players);
-            for(final Player winner: winners) {
+            for (final Player winner : winners) {
                 int currentWinAmount = playersStatistic.get(winner);
                 playersStatistic.put(winner, ++currentWinAmount);
             }
@@ -28,29 +34,40 @@ public class GameStatistic {
         }
     }
 
+    /**
+     * Сброс параметров игроков после каждой игры в начальное состояние
+     */
     private static void clearPlayerInfo() {
-        for(final Player player : playersStatistic.keySet()) {
+        for (final Player player : playersStatistic.keySet()) {
             player.getUnitStateToUnits().forEach((key, value) -> value.clear());
             player.setCoins(0);
             player.setRace(null);
         }
     }
 
+    /**
+     * Иннициализация тестовых игроков
+     */
     private static List<Player> initPlayers() {
-        return initTestPlayers(2);
+        return initTestPlayers(PLAYERS_AMOUNT);
 
     }
 
+    /**
+     * Заполнение мапы со статистикой
+     */
     private static void initStatisticMap(final List<Player> players) {
-        for(final Player player: players) {
+        for (final Player player : players) {
             playersStatistic.put(player, 0);
         }
     }
 
+    /**
+     * Запись результатов в консоль
+     */
     public static void main(final String[] args) {
-        final int gameAmount = 100;
-        play(gameAmount);
-       playersStatistic.forEach(((player, winAmount) -> {
+        play();
+        playersStatistic.forEach(((player, winAmount) -> {
             System.out.println("PLAYER: " + player.getNickname());
             System.out.println("WIN : " + winAmount);
         }));
