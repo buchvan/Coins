7e5d5ebcd83efaa9baa6804114dc337016bdefed
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-07-29 14:44:27 +0600

    [*] Добавила логгер для модуля сбора статистик, изменила генерацию имени для названия лог-файла

--- src/main/java/io/neolab/internship/coins/server/service/GameStatistic.java
+++ src/main/java/io/neolab/internship/coins/server/service/GameStatistic.java
@@ -1,12 +1,8 @@
 package io.neolab.internship.coins.server.service;
 
 import io.neolab.internship.coins.server.game.player.Player;
+import io.neolab.internship.coins.utils.LoggerFile;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +19,6 @@ public class GameStatistic {
     private static Map<Player, Integer> playersStatistic = new HashMap<>();
     private static final int GAME_AMOUNT = 10;
     private static final int PLAYERS_AMOUNT = 2;
-    private static final String STATISTIC_BASE_FILE_NAME = "game-statistic-";
     private static final String DELIMITER = "\n";
     private static int winCounter = 0;
 
@@ -33,7 +28,6 @@ public class GameStatistic {
         initStatisticMap(players);
         for (int i = 0; i < GAME_AMOUNT; i++) {
             final List<Player> winners;
-            System.out.println("COINS BEFORE GAME PLAYER F1: " + players.get(0).getCoins());
             winners = selfPlayByPlayers(players);
             for (final Player winner : winners) {
                 winCounter++;
@@ -72,75 +66,14 @@ public class GameStatistic {
         }
     }
 
-    /**
-     * Вывести статистику на консоль
-     */
-    private static void writeStatisticToConsole(final String playerStr, final String winStr, final String percentStr) {
-        System.out.print(playerStr);
-        System.out.print(winStr);
-        System.out.print(percentStr);
-    }
-
-    /**
-     * Записать статистику в файл
-     */
-    private static void writeStatisticIntoFile(final FileWriter file, final String playerStr,
-                                               final String winStr, final String percentStr) throws IOException {
-        file.write(playerStr);
-        file.write(winStr);
-        file.write(percentStr);
-    }
-
-    /**
-     * Возвращает строку с именем игрока
-     */
-    private static String getPlayerString(final Player player) {
-        return "PLAYER: " + player.getNickname() + DELIMITER;
-    }
-
-    /**
-     * Возвращает строку с количеством побед игрока
-     */
-    private static String getPlayerWinString(final int winAmount) {
-        return "WIN : " + winAmount + DELIMITER;
-    }
-
-    /**
-     * Возвращает строку с процентом побед
-     */
-    private static String getPlayerWinPercentStr(final double percent) {
-        return "% : " + percent + DELIMITER;
-    }
-
-    /**
-     * Сгенерировать имя файла для записи статистики
-     */
-    private static String generateStatisticFileName() {
-        final SimpleDateFormat formatForDateNow = new SimpleDateFormat("E-yyyy-MM-dd-hh-mm-ss");
-        return STATISTIC_BASE_FILE_NAME + formatForDateNow.format(new Date())
-                .replaceAll(" ", "-");
-    }
-
-    /**
-     * Запись результатов в консоль
-     */
     public static void main(final String[] args) {
         play();
-        final String fileName = generateStatisticFileName();
-        try (final FileWriter file = new FileWriter(new File(fileName))) {
-            playersStatistic.forEach(((player, playerWinAmount) -> {
-                final String playerStr = getPlayerString(player);
-                final String winStr = getPlayerWinString(playerWinAmount);
-                final String percentStr = getPlayerWinPercentStr((double) playerWinAmount * 100 / winCounter);
-                writeStatisticToConsole(playerStr, winStr, percentStr);
-                try {
-                    writeStatisticIntoFile(file, playerStr, winStr, percentStr);
-                } catch (final IOException exception) {
-                    System.out.println("Statistic writing failed");
-                }
-            }));
-        } catch (final IOException exception) {
-            System.out.println("Statistic file failed");
+        try (final LoggerFile ignored = new LoggerFile("game-statistic")) {
+            GameStatisticLogger.printLogStatisticFileLog(ignored.toString());
+            playersStatistic
+                    .forEach(((player, playerWinAmount)
+                            -> GameStatisticLogger.printPlayerStatisticLog(player, playerWinAmount,
+                            (double) playerWinAmount * 100 / winCounter)));
         }
     }
 }
---
+++ src/main/java/io/neolab/internship/coins/server/service/GameStatisticLogger.java
@@ -0,0 +1,52 @@
+package io.neolab.internship.coins.server.service;
+
+import io.neolab.internship.coins.server.game.player.Player;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Класс, содержащий основные функции вывода логов об игровой статистике
+ */
+public class GameStatisticLogger {
+    private static final @NotNull Logger LOGGER = LoggerFactory.getLogger(GameStatisticLogger.class);
+
+    /**
+     * Вывод лога о файле, куда ведётся логгирование
+     *
+     * @param logFileName - имя файла-лога
+     */
+    public static void printLogStatisticFileLog(final @NotNull String logFileName) {
+        LOGGER.debug("* Logging in file {} *", logFileName);
+    }
+
+    /**
+     * Запись в лог информации об игроке
+     */
+    public static void printPlayerLog(final Player player) {
+        LOGGER.info("PLAYER: " + player.getNickname());
+    }
+
+    /**
+     * Запись в лог информации о количестве побед
+     */
+    public static void printPlayerWinAmountLog(final int winAmount) {
+        LOGGER.info("WIN: " + winAmount);
+    }
+
+    /**
+     * Запись в лог информации о проценте побед игрока
+     */
+    public static void printPLayerWinPercentLog(final double percent) {
+        LOGGER.info("WIN: " + percent);
+    }
+
+    /**
+     * Запись в лог полной статистики об игроке
+     */
+    public static void printPlayerStatisticLog(final Player player, final int winAmount, final double percent) {
+        printPlayerLog(player);
+        printPlayerWinAmountLog(winAmount);
+        printPLayerWinPercentLog(percent);
+    }
+}
