5833834d93be2631d0b9776178c3045403463623
@ivan.buchinskij <ivan24413@gmail.com>
2020-07-30 09:16:56 +0600

    [r] refactoring in LoggerFile

--- src/main/java/io/neolab/internship/coins/utils/LoggerFile.java
+++ src/main/java/io/neolab/internship/coins/utils/LoggerFile.java
@@ -1,15 +1,18 @@
 package io.neolab.internship.coins.utils;
 
-import io.neolab.internship.coins.server.service.GameLogger;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LoggerFile implements AutoCloseable {
-    private final String logFileName;
+    private static final @NotNull Logger LOGGER = LoggerFactory.getLogger(LoggerFile.class);
+    private final @NotNull String logFileName;
 
-    public LoggerFile(final String prefix) {
+    public LoggerFile(final @NotNull String prefix) {
         final SimpleDateFormat formatForDateNow = new SimpleDateFormat("E-yyyy-MM-dd-hh-mm-ss-S");
 
         /* генерируем имя файла-лога (prefix__E-yyyy-MM-dd-hh-mm-ss-S) */
@@ -17,12 +20,21 @@ public class LoggerFile implements AutoCloseable {
                 formatForDateNow.format(new Date())
                         .replaceAll(":", "-");
         MDC.put("logFileName", logFileName); // в logback.xml по этому ключу берётся имя файла для лога
-        GameLogger.printLogFileLog(logFileName);
+        printLogFileLog(logFileName);
+    }
+
+    /**
+     * Вывод лога о файле, куда ведётся логгирование
+     *
+     * @param logFileName - имя файла-лога
+     */
+    private static synchronized void printLogFileLog(final @NotNull String logFileName) {
+        LOGGER.debug("* Logging in file {} *", logFileName);
     }
 
     @Override
     public void close() {
-        GameLogger.printLogFileLog(logFileName);
+        printLogFileLog(logFileName);
         MDC.remove("logFileName");
     }
 }
