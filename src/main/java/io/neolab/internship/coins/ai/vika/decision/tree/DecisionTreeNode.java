755dff80065c80e81e26d596bdc0f0dc702f8764
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-08-05 14:43:41 +0600

    [R+] changes in tree node fields

--- src/main/java/io/neolab/internship/coins/ai/vika/decision/tree/DecisionTreeNode.java
+++ src/main/java/io/neolab/internship/coins/ai/vika/decision/tree/DecisionTreeNode.java
@@ -4,17 +4,28 @@ import io.neolab.internship.coins.ai.vika.decision.model.Decision;
 import io.neolab.internship.coins.server.game.Game;
 import io.neolab.internship.coins.server.game.player.Player;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * Узел дерева принятия решений
  */
 public class DecisionTreeNode {
+    //Ссылка на узел-родитель
+    private final DecisionTreeNode parentDecision;
+    //Принятое решение
     private final Decision decision;
+    //Игрок, который принимет решения
     private final Player player;
+    //Состояние игры после принятия решения в этом узле
     private final Game game;
+    //Ссылки на потомков
+    private final List<DecisionTreeNode> childDecisions = new LinkedList<>();
 
-    public DecisionTreeNode(final Decision decision, final Player player, final Game game) {
+    public DecisionTreeNode(final DecisionTreeNode parentDecision, final Decision decision,
+                            final Player player, final Game game) {
+        this.parentDecision = parentDecision;
         this.decision = decision;
         this.player = player;
         this.game = game;
@@ -30,6 +41,14 @@ public class DecisionTreeNode {
 
     public Game getGame() {
         return game;
+    }
+
+    public DecisionTreeNode getParentDecision() {
+        return parentDecision;
+    }
+
+    public List<DecisionTreeNode> getChildDecisions() {
+        return childDecisions;
     }
 
     @Override
