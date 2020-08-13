05db34f5656af8f0865a7d2ecd7776642db9060b
@ivan.buchinskij <ivan24413@gmail.com>
2020-08-13 11:43:52 +0600

    [+] add more information in NodeTree

--- src/main/java/io/neolab/internship/coins/client/bot/ai/bim/model/NodeTree.java
+++ src/main/java/io/neolab/internship/coins/client/bot/ai/bim/model/NodeTree.java
@@ -19,14 +19,19 @@ public class NodeTree {
     private final @Nullable Map<Player, Pair<Integer, Integer>> playerToMaxAndMinCoinsCount; // игрок ->
     // пара (максимально число монет, минимальное число монет)
 
+    private final @Nullable Map<Player, Integer> playerToValueDifference; // для каждого игрока минимум по всем
+    // разностям количеств монет между данным игроком и остальными
+
     @Contract(pure = true)
     public NodeTree(final @NotNull List<Edge> edges, final @Nullable Map<Player, Integer> winsCount,
                     final int casesCount,
-                    final @Nullable Map<Player, Pair<Integer, Integer>> playerToMaxAndMinCoinsCount) {
+                    final @Nullable Map<Player, Pair<Integer, Integer>> playerToMaxAndMinCoinsCount,
+                    final @Nullable Map<Player, Integer> playerToValueDifference) {
         this.edges = edges;
         this.winsCount = winsCount;
         this.casesCount = casesCount;
         this.playerToMaxAndMinCoinsCount = playerToMaxAndMinCoinsCount;
+        this.playerToValueDifference = playerToValueDifference;
     }
 
     public @NotNull List<Edge> getEdges() {
@@ -45,7 +50,10 @@ public class NodeTree {
         return playerToMaxAndMinCoinsCount;
     }
 
-    @Contract(value = "null -> false", pure = true)
+    public @Nullable Map<Player, Integer> getPlayerToValueDifference() {
+        return playerToValueDifference;
+    }
+
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
@@ -54,11 +62,12 @@ public class NodeTree {
         return casesCount == nodeTree.casesCount &&
                 edges.equals(nodeTree.edges) &&
                 Objects.equals(winsCount, nodeTree.winsCount) &&
-                Objects.equals(playerToMaxAndMinCoinsCount, nodeTree.playerToMaxAndMinCoinsCount);
+                Objects.equals(playerToMaxAndMinCoinsCount, nodeTree.playerToMaxAndMinCoinsCount) &&
+                Objects.equals(playerToValueDifference, nodeTree.playerToValueDifference);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(edges, winsCount, casesCount, playerToMaxAndMinCoinsCount);
+        return Objects.hash(edges, winsCount, casesCount, playerToMaxAndMinCoinsCount, playerToValueDifference);
     }
 }
