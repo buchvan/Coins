67a944604af178d6de7b2ecf56b3c09dcc3d40b9
@ivan.buchinskij <ivan24413@gmail.com>
2020-08-21 10:17:19 +0600

    [r] refactoring

--- src/main/java/io/neolab/internship/coins/client/SmartClient.java
+++ src/main/java/io/neolab/internship/coins/client/SmartClient.java
@@ -2,7 +2,6 @@ package io.neolab.internship.coins.client;
 
 import io.neolab.internship.coins.client.bot.SmartBot;
 import io.neolab.internship.coins.client.bot.FunctionType;
-import io.neolab.internship.coins.exceptions.CoinsErrorCode;
 import io.neolab.internship.coins.exceptions.CoinsException;
 import org.jetbrains.annotations.NotNull;
 
@@ -40,48 +39,14 @@ public class SmartClient extends Client {
             System.out.println("--- 7. MAX_VALUE_DIFFERENCE");
             System.out.println("--- 8. MIN_MAX_VALUE_DIFFERENCE");
             System.out.println();
-            final int choose = Integer.parseInt(keyboardReader.readLine());
+            final int botType = Integer.parseInt(keyboardReader.readLine());
             System.out.println("Enter max depth of bot: ");
             final int botMaxDepth = Integer.parseInt(keyboardReader.readLine());
-            final Client client;
-            switch (choose) {
-                case 1:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MAX_PERCENT, botMaxDepth);
-                    break;
-                case 2:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MIN_PERCENT, botMaxDepth);
-                    break;
-                case 3:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MIN_MAX_PERCENT, botMaxDepth);
-                    break;
-                case 4:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MAX_VALUE, botMaxDepth);
-                    break;
-                case 5:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MIN_VALUE, botMaxDepth);
-                    break;
-                case 6:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MIN_MAX_VALUE, botMaxDepth);
-                    break;
-                case 7:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MAX_VALUE_DIFFERENCE, botMaxDepth);
-                    break;
-                case 8:
-                    client = new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
-                            FunctionType.MIN_MAX_VALUE_DIFFERENCE, botMaxDepth);
-                    break;
-                default:
-                    throw new CoinsException(CoinsErrorCode.MESSAGE_TYPE_NOT_FOUND);
-            }
+            final Client client =
+                    new SmartClient(clientConfig.getHost(), clientConfig.getPort(),
+                            FunctionType.values()[botType - 1], botMaxDepth);
             client.startClient();
-        } catch (final CoinsException | IOException exception) {
+        } catch (final CoinsException | IOException | NullPointerException exception) {
             LOGGER.error("Error!", exception);
         }
     }
